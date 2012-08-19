63be283bc6730bf1168977688ca1827e0d104b8b
MORI Shingo <babydaemons@gmail.com>
2012-08-19 19:50:07 +0900

    implemented script extract wiki-pages.

---
+++ extractPages.pl
@@ -0,0 +1,29 @@
+#!/usr/bin/perl
+
+my $input = $ARGV[0];
+open(IN, '<', $input) or die "$0: $!: $input";
+my $outdir_root = $input; $outdir_root =~ s/\.xml$//;
+mkdir $outdir_root unless -d $outdir_root;
+my $outdir_base = "$outdir_root/%04d";
+my $output_base = "$outdir_base/%04d.xml";
+
+my $i = 0;
+my @lines;
+while (<IN>) {
+  s/  </</;
+  if ($_ eq "<page>\n") {
+    @lines = ();
+  }
+  push(@lines, $_);
+  if ($_ eq "</page>\n") {
+    my $n = int($i / 1000);
+    my $m = int($i % 1000);
+    my $outdir = sprintf($outdir_base, $n);
+    my $output = sprintf($output_base, $n, $m);
+    mkdir $outdir unless -d $outdir;
+    open(OUT, '>', $output) or die "$0: $!: $output";
+    print OUT join("" => @lines);
+    ++$i;
+    @lines = ();
+  }
+}
